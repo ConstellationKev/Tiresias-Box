e54a4c70cce448938e05b0652725f5730c534b92
ConstellationKev <kevinkunzhong@gmail.com>
2026-06-13 13:03:50 -0700

    safe power off servos for setup

--- firmware/hardware_setup.py
+++ firmware/hardware_setup.py
@@ -5,3 +5,6 @@ kit = ServoKit(channels=16)
 # setting servos to 0 position to attach the gears; tune these values later depending on the gears
 kit.servo[0].angle = 0 #inner
 kit.servo[3].angle = 0 #outer
+
+kit.servo[0].angle = None #inner
+kit.servo[3].angle = None #outer
